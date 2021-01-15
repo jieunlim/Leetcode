e6912ede949e5559267e1319b595ddaa624b389e
jieunlim <jieun.lim200@gmail.com>
2021-01-15 00:45:54 -0800

    completed 1

---
+++ 1-TwoSum.py
@@ -0,0 +1,12 @@
+class Solution:
+    def twoSum(self, nums, target) :
+        
+        for i in range(len(nums)):
+            
+            for j in range(i+1, len(nums)):
+                if nums [j] == target - nums[i]:
+                    return [i, j]
+obj = Solution()
+nums = [2,7,11,15]
+target = 9
+print(obj.twoSum(nums, target))
