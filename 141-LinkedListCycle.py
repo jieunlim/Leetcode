d731053e76ea22eef73bedec795ac1f70c21ec8e
jieunlim <jieun.lim200@gmail.com>
2021-01-21 22:30:01 -0800

    set & rabbit turtle

---
+++ 141-LinkedListCycle.py
@@ -0,0 +1,46 @@
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+# s: o(1)
+# t: o(n)
+# https://leetcode.com/problems/linked-list-cycle/discuss/44539/AC-Python-76ms-Floyd-loop-detection-in-7-lines
+# Floyd's Cycle Detection Alogrithm or Tortoise and Hare Algorithm
+class Solution:
+    def hasCycle(self, head):
+        
+        fast = head
+        slow = head
+        
+        while fast and fast.next:
+            fast = fast.next.next
+            slow = slow. next 
+            
+            if fast == slow:
+                return True
+       
+        return False
+    def hasCycle2(self, head):
+        #s: o(n)
+        #t: o(n)
+        s = set()
+        while head and head.next:
+            if head in s:
+                return True
+            s.add(head)
+            head = head.next
+        return False
+        
+head = ListNode(1)
+l2 = ListNode(2)
+l3 = ListNode(3)
+l4 = ListNode(4)
+
+head.next = l2
+l2.next = l3
+l3.next = l4
+l4.next = l2
+
+obj = Solution()
+print(obj.hasCycle(head))
