1773c5fd0b131a7d4f311110a58cde2755c7e2f9
jieunlim <zhiyinde@gmail.com>
2021-11-08 22:57:00 -0800

    sort using lambda

--- 1710-MaximumUnitsonaTruck.py
+++ 1710-MaximumUnitsonaTruck.py
@@ -15,3 +15,22 @@ obj = Solution()
 boxTypes = [[1,3],[2,2],[3,1]]
 truckSize = 4
 print(obj.maximumUnits(boxTypes, truckSize))
+
+class Solution2:
+    def maximumUnits(self, boxTypes: List[List[int]], truckSize: int) -> int:
+        ans = 0
+        while truckSize>0:
+            maxUnit = 0
+            index = 0
+            
+            for i in range(len(boxTypes)):
+                if boxTypes[i][1]>maxUnit:
+                    index = i
+                    maxUnit = boxTypes[i][1]
+            ans += min(boxTypes[index][0], truckSize) * maxUnit
+            truckSize-=boxTypes[index][0]
+            boxTypes[index][1]=0
+            
+        return ans
+                
+                
