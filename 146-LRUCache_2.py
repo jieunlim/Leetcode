33d8a0eb21b9b0d953200d9dd3a5aa140c222c52
Jieun Lim <zhiyinde@gmail.com>
2022-04-20 00:20:16 -0700

    Create 146-LRUCache_2.py
    Ordered dictionary

---
+++ 146-LRUCache_2.py
@@ -0,0 +1,33 @@
+#Could you do both operations in O(1) time complexity?
+
+# Ordered dictionary
+# Python - OrderedDict
+# Java - LinkedHashMap
+# time - O(1), space - O(capacity)
+# 1) get(key) - return value /-1 if not exists in the
+# cache
+# 2) put(key, value) - set or insert/ invalidate LRU
+# item if the cache reached it’s capacity 
+  
+   def get(self, key: int) -> int:
+        
+        if key not in self:
+            return -1
+        
+        self.move_to_end(key)
+        return self[key]
+
+    def put(self, key: int, value: int) -> None:
+        if key in self:
+            self.move_to_end(key)
+        self[key] = value
+            
+        if len(self) > self.capacity:
+                self.popitem(last = False) #FIFO
+            
+
+
+# Your LRUCache object will be instantiated and called as such:
+# obj = LRUCache(capacity)
+# param_1 = obj.get(key)
+# obj.put(key,value)
