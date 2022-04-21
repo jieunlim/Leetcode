5bd7acb61fa400040e3b03602836cf19609f2b06
Jieun Lim <zhiyinde@gmail.com>
2022-04-20 22:50:07 -0700

    Update 2104-SumofSubarrayRanges.py

--- 2104-SumofSubarrayRanges.py
+++ 2104-SumofSubarrayRanges.py
@@ -13,3 +13,21 @@ class Solution:
                 maxx = max(maxx, nums[j])
                 sol += maxx - minn
         return sol
+
+#T: O(nlogn) S: n!
+import heapq
+
+class Solution2:
+    def subArrayRanges(self, nums: List[int]) -> int:
+        n = len(nums)
+        sol = 0
+        
+        for i in range(n):#n
+            maxx = [-nums[i]] #max heap
+            minn = [nums[i]] #min heap
+        
+            for j in range(i, n): #log(n)
+                heapq.heappush(maxx, -nums[j])
+                heapq.heappush(minn, nums[j])
+                sol += -maxx[0] - minn[0]
+        return sol
