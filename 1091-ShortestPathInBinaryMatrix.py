fbee1b9f2f0a4abe3608da4c00c8cde9b64656c4
Jieun Lim <zhiyinde@gmail.com>
2022-05-15 17:09:30 -0700

    Update 1091-ShortestPathInBinaryMatrix.py

--- 1091-ShortestPathInBinaryMatrix.py
+++ 1091-ShortestPathInBinaryMatrix.py
@@ -1,4 +1,6 @@
 
+
+
 class Solution:
     def shortestPathBinaryMatrix(self, grid: List[List[int]]) -> int:
         rows = len(grid)-1
@@ -24,7 +26,27 @@ class Solution:
             else: helper(r, c, path)
                 
         return -1
+    
+    
+class Solution1:
+    def shortestPathBinaryMatrix(self, grid: List[List[int]]) -> int:
+        rows = len(grid)
+        cols = len(grid[0])
+        if grid[0][0] == 1:
+            return -1
 
+        q = [(0, 0, 1)]
+        while len(q) > 0:
+            x, y, d = q.pop(0)
+            if x == rows-1 and y == cols-1:
+                return d
+            for a, b in ((x-1, y-1), (x+1, y+1), (x-1, y), (x+1, y), (x, y-1), (x, y+1), (x-1, y+1), (x+1, y-1)):
+                if 0 <= a < rows and 0 <= b < cols and grid[a][b] == 0:
+                    grid[a][b] = 1
+                    q.append((a, b, d+1))
+
+        return -1
+    
     
 class Solution2:
     def shortestPathBinaryMatrix(self, grid: List[List[int]]) -> int:
@@ -53,3 +75,5 @@ class Solution2:
                         grid[new_row][new_col] = 1
                 
         return -1
+    
+
