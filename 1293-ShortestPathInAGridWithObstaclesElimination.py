6129dc3b5009b4a14227912ba5dd1847beaca77c
Jieun Lim <zhiyinde@gmail.com>
2022-06-14 00:35:25 -0700

    Update 1293-ShortestPathInAGridWithObstaclesElimination.py

--- 1293-ShortestPathInAGridWithObstaclesElimination.py
+++ 1293-ShortestPathInAGridWithObstaclesElimination.py
@@ -21,3 +21,31 @@ class Solution:
                             visited.add((nr,nc,obstaclecnt))
                             queue.append((nr,nc,obstaclecnt, stepcnt+1))
         return -1 #not possible
+    
+    
+from collections import deque
+class Solution2:
+    def shortestPath(self, grid: List[List[int]], k: int) -> int:
+        m, n = len(grid), len(grid[0])
+        direction = [(0,1), (0,-1), (1,0), (-1,0)] # right, left, up, down
+        q = deque([])
+        q.append((0, 0, 0, k)) # row, col, step, k
+        seen = set()
+        
+        while q:
+            r, c, step, rk = q.popleft()
+            if r == m-1 and c == n-1: return step
+            else:
+                for x, y in direction:
+                    nr, nc = r + x, c + y
+                    if 0 <= nr < m and 0 <= nc < n and (nr, nc, rk) not in seen:
+                        if grid[nr][nc] and rk > 0: #blocked
+                            seen.add((nr, nc, rk))
+                            q.append((nr, nc, step+1, rk-1))
+                        elif grid[nr][nc] == 0:
+                            seen.add((nr, nc, rk))
+                            q.append((nr, nc, step+1, rk))
+        return -1
+                        
+                        
+            
