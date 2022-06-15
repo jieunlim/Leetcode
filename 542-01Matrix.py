1aebc1912807d8a3b3377f93effb8774d0f749e1
Jieun Lim <zhiyinde@gmail.com>
2022-06-15 01:22:09 -0700

    Create 542-01Matrix.py
    matrix

---
+++ 542-01Matrix.py
@@ -0,0 +1,28 @@
+#T: O(nm), S: O(mn) - maintain Queue
+from collections import deque
+class Solution:
+    def updateMatrix(self, mat: List[List[int]]) -> List[List[int]]:
+        m, n = len(mat), len(mat[0])
+        direction = [(0,1), (0,-1), (-1,0), (1,0)] # right, left, up, down
+        seen = set() #r, c
+        q = deque([])#r, c, distance
+        
+        for i in range(m):
+            for j in range(n):
+                if mat[i][j] == 0:
+                    seen.add((i, j))
+                    q.append((i, j))  
+        
+        
+        while q:
+            r, c = q.popleft()
+           
+            for x, y in direction:
+                nr, nc = x+r, y+c
+                if 0 <= nr < m and 0 <= nc < n and (nr, nc) not in seen:
+                    mat[nr][nc] = mat[r][c] + 1
+                    seen.add((nr, nc))
+                    q.append((nr, nc))
+        return mat
+        
+        
