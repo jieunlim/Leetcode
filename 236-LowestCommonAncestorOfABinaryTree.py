f8d069e84ccaafd267e0fcdef5d6ef81e57eb3f7
Jieun Lim <zhiyinde@gmail.com>
2022-07-08 01:47:03 -0700

    Create 236-LowestCommonAncestorOfABinaryTree.py

---
+++ 236-LowestCommonAncestorOfABinaryTree.py
@@ -0,0 +1,9 @@
+#T:O(n), S: O(n) - recursive stack when skewed binary tree
+class Solution:
+    def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+        if not root: return
+        if root.val == p.val or root.val == q.val: return root
+        left = self.lowestCommonAncestor(root.left, p, q)
+        right = self.lowestCommonAncestor(root.right, p, q)
+        if left and right: return root
+        else: return left or right
