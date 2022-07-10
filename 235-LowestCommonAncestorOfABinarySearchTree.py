0676a447558733d4a43995061e8bda71dd522ece
Jieun Lim <zhiyinde@gmail.com>
2022-07-09 18:19:14 -0700

    Create 235-LowestCommonAncestorOfABinarySearchTree.py

---
+++ 235-LowestCommonAncestorOfABinarySearchTree.py
@@ -0,0 +1,12 @@
+#Coult be same as #236
+#O(N), O(1)
+class Solution:
+    def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+        while root:
+            
+            if p.val > root.val and q.val > root.val:
+                root = root.right
+            elif p.val < root.val and q.val < root.val:
+                root = root.left
+            else:
+                return root
