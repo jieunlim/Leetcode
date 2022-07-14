e183865723b0f678797ef18cab07214a7c4a6328
Jieun Lim <zhiyinde@gmail.com>
2022-07-14 08:20:36 -0700

    Update 301-RemoveInvalidParentheses.py

--- 301-RemoveInvalidParentheses.py
+++ 301-RemoveInvalidParentheses.py
@@ -1,29 +1,33 @@
 class Solution:
     def removeInvalidParentheses(self, s: str) -> List[str]:
-        level = {s}
-        while True:
-            sol = []
-            for ele in level:
-                if self.isValid(ele):
-                    sol.append(ele)
-            if sol:
-                return sol
-            
-            new_sol = set()
-            
-            for ele in level:
-                for i in range(len(ele)):
-                    new_sol.add(ele[:i]+ele[i+1:])
-            level = new_sol
-            
-    def isValid(self, s):
-        count = 0
-        for c in s:
-            if c =="(":
-                count += 1
-            elif c == ")":
-                count -=1
-                if count < 0:
-                    return False
-        return count == 0
+        sol = []
+        cur = set()
+        cur.add(s) 
         
+        while cur:
+            for e in cur:
+                if self.isValid(e):
+                    sol.append(e)
+            if sol: return sol
+
+
+            new_cur = set()
+            for e in cur:
+                for i in range(len(e)):
+                    new_cur.add(e[:i]+e[i+1:])
+            cur = new_cur
+
+        
+    def isValid(self, s):
+        stack = []
+        for ch in s:
+            if ch not in ['(',')']:
+                continue
+            elif ch == '(':
+                stack.append(ch)
+            elif stack and ch == ')':
+                stack.pop()
+            else: return False
+        return stack == []
+    
+            
