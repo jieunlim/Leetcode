82c27c47fb6038418db26ed2dcaff1756643bb9d
Jieun Lim <zhiyinde@gmail.com>
2022-08-07 19:19:01 -0700

    Update 670-MaximumSwap.py

--- 670-MaximumSwap.py
+++ 670-MaximumSwap.py
@@ -28,3 +28,25 @@ class Solution:
 
 
         return sol
+    
+    
+class Solution2:
+    def maximumSwap(self, num: int) -> int:
+        arr = [(i, int(x)) for i, x in enumerate(str(num))]
+        sort_arr = sorted(arr, key = lambda x:x[1], reverse= True)
+        
+        for i in range(len(arr)):
+            if sort_arr[i][1] == arr[i][1]: continue
+            else:
+                find = sort_arr[i][1]
+                for j in range(len(sort_arr)-1, -1, -1):
+                    if sort_arr[j][1] == find:
+                        arr[i], arr[sort_arr[j][0]] = arr[sort_arr[j][0]], arr[i]
+                        break
+            break
+        sol = 0
+        
+        for i, n in arr:
+            sol = sol * 10 + n
+        return sol
+            
