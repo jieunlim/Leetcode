2e123f29444d968a1c6d3c2370d1d73c25237559
Jieun Lim <zhiyinde@gmail.com>
2022-08-07 17:59:34 -0700

    Update 791-CustomSortString.py

--- 791-CustomSortString.py
+++ 791-CustomSortString.py
@@ -10,3 +10,22 @@ class Solution:
         for c in cnt:
             sol.append(c * cnt[c])
         return ''.join(sol)
+    
+class Solution2:
+    def customSortString(self, order: str, s: str) -> str:
+        dic = {}
+        cnt = Counter(s)
+        in_ordered = ['']*len(s)
+        not_in_ordered=['']*len(s)
+        
+        for i, ch in enumerate(order):
+            dic[ch] = i
+        
+        for i, ch in enumerate(s):
+            
+            if ch in order:
+                in_ordered[dic[ch]] = ch * cnt[ch]
+            else: not_in_ordered.append(ch)
+        
+        return ''.join(in_ordered) + ''.join(not_in_ordered)
+                
