c2ef82982cb0f646aae8acb88e56ad3eedd34edc
Jieun Lim <zhiyinde@gmail.com>
2022-08-09 00:18:55 -0700

    Update 246-StrobogrammaticNumber.py

--- 246-StrobogrammaticNumber.py
+++ 246-StrobogrammaticNumber.py
@@ -1,5 +1,23 @@
 class Solution:
     def isStrobogrammatic(self, num: str) -> bool:
+        num = str(num)
+        dic = {'8': '8', '1': '1', '0':'0', '6': '9', '9': '6'}
+        
+        l, r = 0, len(num)-1
+        
+        while l<r:
+            if num[l] not in dic: return False
+            elif dic[num[l]] == num[r]:
+                l+=1
+                r-=1
+            else: return False
+        if l == r:
+            if num[l] in '810': return True
+            else: return False
+        return True
+
+class Solution2:
+    def isStrobogrammatic(self, num: str) -> bool:
         l, r = 0, len(num)-1
         num = str(num)
         same = ['8','1','0']
