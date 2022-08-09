b876c0db37560049add44e83d694eb26ea5d41e2
Jieun Lim <zhiyinde@gmail.com>
2022-08-08 23:45:26 -0700

    Update 398-RandomPickIndex.py

--- 398-RandomPickIndex.py
+++ 398-RandomPickIndex.py
@@ -9,4 +9,4 @@ class Solution:
         if len(self.dic[target])==1: return self.dic[target][0]
         else:
             n = random.randrange(0, len(self.dic[target]))
-            return self.dic[target][n]
+            return self.dic[target][n] #return random.choice(self.dic[target])
