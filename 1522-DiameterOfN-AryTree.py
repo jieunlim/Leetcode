6faeb749d8584821f7db26d732c4c92de8c12037
Jieun Lim <zhiyinde@gmail.com>
2022-08-13 01:04:46 -0700

    Create 1522-DiameterOfN-AryTree.py

---
+++ 1522-DiameterOfN-AryTree.py
@@ -0,0 +1,26 @@
+class Solution:
+    def diameter(self, root: 'Node') -> int:
+        """
+        :type root: 'Node'
+        :rtype: int
+        """
+        res = 0
+        
+        def helper(node):
+            if not node: return 0
+            nonlocal res
+            first = second = 0
+            
+            
+            for child in node.children:
+                depth = helper(child)
+                if depth > first:
+                    first, second = depth, first
+                elif depth > second:
+                    second = depth
+            res = max(res, first+ second)
+            return first + 1
+            
+        helper(root)
+        return res
+        
