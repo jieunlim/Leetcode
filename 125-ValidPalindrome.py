c5cc6578ebea3092647a11f53478124aeb048417
Jieun Lim <zhiyinde@gmail.com>
2022-08-12 22:51:51 -0700

    Update 125-ValidPalindrome.py

--- 125-ValidPalindrome.py
+++ 125-ValidPalindrome.py
@@ -1,19 +1,41 @@
 #O(n), O(1)
 class Solution:
-    def isPalindrome(self, s):
-        i, j = 0, len(s)-1
-        
-        while i<j:
-            if not s[i].isalpha():
-                i+=1
-            elif not s[j].isalphe():
-                j-=1
-            elif s[i].lower()!= s[j].lower():
-                return False
+    def isPalindrome(self, s: str) -> bool:
+        l, r = 0, len(s)-1
+        while l<r:
+            print(s[l], s[r])
+            if not s[l].isalpha() and not s[l].isdigit():
+                l+=1
+                continue
+            if not s[r].isalpha() and not s[r].isdigit():
+                r-=1
+                continue
+            if s[l].lower() == s[r].lower():
+                l+=1
+                r-=1
             else:
-                i+=1
-                j-=1
+                return False
         return True
+    
+class Solution2:
+    def isPalindrome(self, s: str) -> bool:
+        l, r = 0, len(s)-1
+        while l<r:
+            
+            if not s[l].isalnum():
+                l+=1
+                continue
+            if not s[r].isalnum():
+                r-=1
+                continue
+            if s[l].lower() == s[r].lower():
+                l+=1
+                r-=1
+            else:
+                return False
+        return True
+            
+            
 obj = Solution()
 s = "12321"
 print(obj.isPalindrome(s))
