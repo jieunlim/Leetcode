4e3ca6c65f85af9441270227720cdce91ea73a7e
Jieun Lim <zhiyinde@gmail.com>
2022-08-15 23:56:01 -0700

    Update 1060-MissingElementInSortedArray.py

--- 1060-MissingElementInSortedArray.py
+++ 1060-MissingElementInSortedArray.py
@@ -1,16 +1,16 @@
 class Solution:
     def missingElement(self, nums: List[int], k: int) -> int:
-        misCnt = []
+        cnt = []
         for i, n in enumerate(nums):
-            misCnt.append(nums[i] - nums[0] - i)
+            cnt.append(n-nums[0]-i)
         
-        if k > misCnt[-1]: return nums[-1] - misCnt[-1] + k
+        if k > cnt[-1]:
+            return nums[-1] + k - cnt[-1]
         
-        missing, idx = 0, 0
-        
-        for i, cnt in enumerate(misCnt):
-            if cnt >= k: break
-            missing = cnt
-            idx = i 
-        return nums[idx] - misCnt[idx] + k
-           
+        for i, n in enumerate(cnt):
+            if k <= n:
+                break
+            idx = i
+            
+        return nums[idx] + k - cnt[idx]
+  
