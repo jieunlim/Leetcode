41616f925ca29c65db6ab44c0912fcac3782b6f3
Jieun Lim <zhiyinde@gmail.com>
2022-08-21 20:53:03 -0700

    Update 298-BinaryTreeLongestConsecutiveSequence.py

--- 298-BinaryTreeLongestConsecutiveSequence.py
+++ 298-BinaryTreeLongestConsecutiveSequence.py
@@ -20,3 +20,27 @@ class Solution:
         
         helper(root, 1)
         return self.maxVal
+
+class Solution2:
+    def longestConsecutive(self, root: Optional[TreeNode]) -> int:
+        self.maxVal = 0
+        def helper(node, cnt):
+            if not node: return 0
+            self.maxVal = max(cnt, self.maxVal)    
+            if node.left:
+                if node.val + 1 == node.left.val:
+                    helper(node.left, cnt+1)
+                else:
+                    helper(node.left, 1)
+                
+            if node.right:
+                if node.val + 1 == node.right.val:
+                    helper(node.right, cnt+1)
+                else: 
+                    helper(node.right, 1)
+           
+        helper(root, 1)
+        return self.maxVal
+        
+        
+        
