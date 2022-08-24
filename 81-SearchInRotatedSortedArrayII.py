3c82116c0393358d82cacc41e183f631957169ef
J <zhiyinde@gmail.com>
2022-08-24 01:26:44 -0700

    Update 81-SearchInRotatedSortedArrayII.py

--- 81-SearchInRotatedSortedArrayII.py
+++ 81-SearchInRotatedSortedArrayII.py
@@ -4,6 +4,33 @@ class Solution:
         
         while l <= r:
             
+            m = l + (r - l)//2
+            while l < m and nums[l] == nums[m]:
+                l += 1
+                
+            
+            if target == nums[m]: return True
+                
+            if nums[l] <= nums[m]:
+                if nums[l] <= target < nums[m]:
+                    r = m - 1
+                else:
+                    l = m + 1
+            else:
+                if nums[m] < target <= nums[r]:
+                    l = m + 1
+                else:
+                    r = m - 1
+                    
+        return False
+        
+
+class Solution2:
+    def search(self, nums: List[int], target: int) -> bool:
+        l, r = 0, len(nums)-1
+        
+        while l <= r:
+            
             while l < r and nums[l] == nums[l + 1]:
                 l += 1
             while l < r and nums[r] == nums[r - 1]:
