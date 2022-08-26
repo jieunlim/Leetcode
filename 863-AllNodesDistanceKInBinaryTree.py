22c1b288bc891f00879cdba6b12ac6ebde95ff02
J <zhiyinde@gmail.com>
2022-08-26 00:00:38 -0700

    Update 863-AllNodesDistanceKInBinaryTree.py

--- 863-AllNodesDistanceKInBinaryTree.py
+++ 863-AllNodesDistanceKInBinaryTree.py
@@ -3,8 +3,35 @@
 #         self.val = x
 #         self.left = None
 #         self.right = None
-
+#O(n), O(n)
 class Solution:
+    def distanceK(self, root: TreeNode, target: TreeNode, k: int) -> List[int]:
+        parent = {}
+        def build(node, p = None):
+            if not node: return
+            parent[node] = p
+            build(node.left, node)
+            build(node.right, node)
+        
+        build(root)
+        
+        queue = deque([(target, k)])
+        seen = set()
+        res = []
+        
+        while queue:
+            node, k = queue.popleft()
+            if node:
+                if k == 0: res.append(node.val)
+                seen.add(node)
+                for n in (node.left, node.right, parent[node]):
+                    if n not in seen:
+                        queue.append((n, k-1))
+        return res
+
+    
+    
+class Solution2:
     def distanceK(self, root: TreeNode, target: TreeNode, k: int) -> List[int]:
         
         adj = defaultdict(list) # node: neighbors
