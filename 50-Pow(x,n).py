646a8b0916dd84e57035d848f0668ec17d8c12f8
J <zhiyinde@gmail.com>
2022-08-26 23:49:00 -0700

    Update 50-Pow(x,n).py

--- 50-Pow(x,n).py
+++ 50-Pow(x,n).py
@@ -1,6 +1,17 @@
 class Solution:
     def myPow(self, x: float, n: int) -> float:
         
+        if n == 0:
+            return 1
+        if n < 0:
+            n = abs(n)
+            x = 1/x
+        return self.myPow(x*x, n//2) if n%2==0 else x*self.myPow(x*x, n//2)
+
+
+class Solution2:
+    def myPow(self, x: float, n: int) -> float:
+        
         def helper(x, n):
             if n == 0:
                 return 1
