0cb20c14642529c089c1cbee1b6a4af0cb292869
J <zhiyinde@gmail.com>
2022-08-28 16:47:18 -0700

    Update 311-SparseMatrixMultiplication.py

--- 311-SparseMatrixMultiplication.py
+++ 311-SparseMatrixMultiplication.py
@@ -1,6 +1,27 @@
-#dic 사용, 0이 아닌수만 곱해주자.
+#dic/set 사용, 0이 아닌수만 곱해주자.
+class Solution1:
+    def multiply(self, mat1: List[List[int]], mat2: List[List[int]]) -> List[List[int]]:
+        mat3 = [[0 for _ in range(len(mat2[0]))] for _ in range(len(mat1))]
+        dic1, dic2 = set(), set()
+        for i in range(len(mat1)):
+            for j in range(len(mat1[0])):
+                if mat1[i][j] !=0: 
+                    dic1.add((i,j))
+                
+        for i in range(len(mat2)):
+            for j in range(len(mat2[0])):
+                if mat2[i][j] !=0: 
+                    dic2.add((i,j))
+                
+        for i in range(len(mat1)):
+            for j in range(len(mat2[0])):
+                for k in range(len(mat1[0])):
+                    a = mat1[i][k] if (i,k) in dic1 else 0
+                    b = mat2[k][j] if (k,j) in dic2 else 0
+                    mat3[i][j] += a * b
+        return mat3
 
-class Solution:
+class Solution2:
     def multiply(self, mat1: List[List[int]], mat2: List[List[int]]) -> List[List[int]]:
         #res = [[0 for _ in range(len(mat2[0]))] for _ in range(len(mat1))]
         res = [[0]*len(mat2[0]) for _ in range(len(mat1))]  # len(mat1) * len(mat2[0])
@@ -31,3 +52,4 @@ class Solution:
                 
         return res
         
+
