34432d22853aba922b1e048e86516764ce55a454
J <zhiyinde@gmail.com>
2022-08-28 00:16:13 -0700

    Update 398-RandomPickIndex.py

--- 398-RandomPickIndex.py
+++ 398-RandomPickIndex.py
@@ -1,11 +1,11 @@
 class Solution:
 
-    def __init__(self, nums: List[int]):
+    def __init__(self, nums: List[int]): #O(n)
         self.dic = defaultdict(list)
         for i, num in enumerate(nums):
             self.dic[num].append(i)
 
-    def pick(self, target: int) -> int:
+    def pick(self, target: int) -> int: # O(1)
         if len(self.dic[target])==1: return self.dic[target][0]
         else:
             n = random.randrange(0, len(self.dic[target]))
