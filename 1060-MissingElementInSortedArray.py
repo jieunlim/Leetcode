d00ef0cddccadb090fadf2a7863fd7855fdff734
J <zhiyinde@gmail.com>
2022-08-29 00:50:22 -0700

    binary search - 1539

--- 1060-MissingElementInSortedArray.py
+++ 1060-MissingElementInSortedArray.py
@@ -1,3 +1,21 @@
+#O(logn), binary search
+
+class Solution:
+    def missingElement(self, nums: List[int], k: int) -> int:
+        
+        
+        l, r = 0, len(nums) - 1
+
+        while l <= r:
+            mid = (l + r) // 2
+            if nums[mid] - nums[0] - mid < k:  
+                l = mid + 1
+            else:
+                r = mid - 1
+        # return nums[0] + k + l
+        
+        return nums[r] + k - (nums[r] - nums[0] - r)  
+
 class Solution:
     def missingElement(self, nums: List[int], k: int) -> int:
         cnt = []
