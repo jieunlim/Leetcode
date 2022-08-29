ad0cf7cbaa42e5b5b1ad32acc8e91b10163091ee
J <zhiyinde@gmail.com>
2022-08-28 23:41:01 -0700

    1060, 173 binary search

--- 1539-KthMissingPositiveNumber.py
+++ 1539-KthMissingPositiveNumber.py
@@ -1,3 +1,20 @@
+# O(log n) / O(1)
+class Solution:
+    def findKthPositive(self, arr: List[int], k: int) -> int:
+        l, r = 0, len(arr)-1
+        
+        while l <= r:
+            m = l + (r - l)//2
+            missing = arr[m] - m - 1
+            if missing < k:
+                l = m + 1
+            else:
+                r = m - 1
+                
+                
+        return arr[r] + (k - (arr[r] - r - 1)) # k + r - 1
+            
+# O(n) / O(1)
 class Solution:
     def findKthPositive(self, arr: List[int], k: int) -> int:
         idx = 1
