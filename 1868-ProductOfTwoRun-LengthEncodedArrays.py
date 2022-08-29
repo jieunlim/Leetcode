79642f85b4a459241fcafd560aa4aa3593c06376
J <zhiyinde@gmail.com>
2022-08-28 19:52:57 -0700

    Update 1868-ProductOfTwoRun-LengthEncodedArrays.py

--- 1868-ProductOfTwoRun-LengthEncodedArrays.py
+++ 1868-ProductOfTwoRun-LengthEncodedArrays.py
@@ -1,30 +1,55 @@
+#T: O(n), S: O(1) 
 class Solution:
     def findRLEArray(self, encoded1: List[List[int]], encoded2: List[List[int]]) -> List[List[int]]:
+        res = []
         
-        product = []
+        i = j = 0
         
-        e1 = e2 = 0
-        
-        while e1 < len(encoded1) and e2<len(encoded2):
-            e1_val, e1_cnt = encoded1[e1] #[[1,0],[2,1],[3,2]]
-            e2_val, e2_cnt = encoded2[e2] #[[2,0],[3,3]]
+        while i<len(encoded1) and j<len(encoded2):
+            v1, c1 = encoded1[i]
+            v2, c2 = encoded2[j]
             
-            product_val = e1_val * e2_val # 2
-            product_cnt = min(e1_cnt, e2_cnt) #3
+            p = v1 * v2
+            cnt = min(c1, c2)
             
-            
-            encoded1[e1][1] -= product_cnt
-            encoded2[e2][1] -= product_cnt
-            
-            if encoded1[e1][1] == 0:
-                e1 += 1
-            
-            if encoded2[e2][1] == 0:
-                e2 += 1
-            
-            if not product or product[-1][0] != product_val:
-                    product.append([product_val, product_cnt])
-            else: product[-1][1] += product_cnt
+            encoded1[i][1] -= cnt
+            encoded2[j][1] -= cnt
                 
-        return product
-                    
+            if encoded1[i][1] == 0:
+                i += 1
+            if encoded2[j][1] == 0:
+                j += 1
+            
+            if not res or res[-1][0] != p:
+                res.append([p, cnt])
+            else: res[-1][1] += cnt
+                
+            
+            
+        return res
+    
+#TLE
+class Solution:
+    def findRLEArray(self, encoded1: List[List[int]], encoded2: List[List[int]]) -> List[List[int]]:
+        l1, l2 = [], []
+        res = []
+        for num, cnt in encoded1: 
+            for _ in range(cnt):
+                l1.append(num)
+        for num, cnt in encoded2: 
+            for _ in range(cnt):
+                l2.append(num)
+        
+        cur = []
+        for a, b in zip(l1, l2):
+                cur.append(a * b)
+        
+        cnt = 0
+        res.append([cur[0], 0])
+        for i in range(len(cur)):
+            if cur[i] == res[-1][0]:
+                res[-1][1]+=1
+            else:
+                res.append([cur[i], 1])
+        return res
+        
