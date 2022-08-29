29f8aecf1b5e98ddf5c75c4ac9ca03216794ffce
J <zhiyinde@gmail.com>
2022-08-28 18:13:02 -0700

    Update 704-BinarySearch.py

--- 704-BinarySearch.py
+++ 704-BinarySearch.py
@@ -19,3 +19,15 @@ class Solution:
             else: l = m + 1
                 
         return r if nums[r] == target else -1
+    
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        l, r = 0, len(nums)-1
+        while l <= r:
+            m = l + (r - l)//2
+            if nums[m] == target: return m
+            if nums[m] > target:
+                r = m - 1
+            else: l = m + 1
+                
+        return -1
