157de375b5754fa99a998d92e99b9b56f9e48b33
J <zhiyinde@gmail.com>
2022-08-29 23:27:04 -0700

    Update 133-CloneGraph.py

--- 133-CloneGraph.py
+++ 133-CloneGraph.py
@@ -2,7 +2,7 @@ class Node:
     def __init__(self, val = 0, neighbors = None):
         self.val = val
         self.neighbors = neighbors if neighbors is not None else []
-#dfs
+#dfs - recursion
 class Solution:
     def cloneGraph(self, node: 'Node') -> 'Node':
         if not node: return 
@@ -20,6 +20,30 @@ class Solution:
             return visited[node]
         
         return dfs(node)
+    
+    
+#dfs - iterative  
+
+class Solution:
+    def cloneGraph(self, node: 'Node') -> 'Node':
+        if not node: return
+        stack = [node]
+        m = {}
+        visited = set()
+        
+        while stack:
+            n = stack.pop()
+            if n in visited: continue
+            else: visited.add(n)
+            if n not in m:
+                m[n] = Node(n.val)
+            for neigh in n.neighbors:
+                if neigh not in m:
+                    m[neigh] = Node(neigh.val)
+                m[n].neighbors.append(m[neigh])
+                stack.append(neigh)
+                
+        return m[node]
     
  #bfs
 class Solution2:
