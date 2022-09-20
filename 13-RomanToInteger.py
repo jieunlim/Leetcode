c19189fec10502721626dcfd90be57f3d93ec946
J <zhiyinde@gmail.com>
2022-09-19 21:18:20 -0700

    Update 13-RomanToInteger.py

--- 13-RomanToInteger.py
+++ 13-RomanToInteger.py
@@ -21,5 +21,20 @@ class Solution:
             res += dic[s[i]]
                 
         return res
+    
+class Solution:
+    def romanToInt(self, s: str) -> int:
+        res = 0
+        dic = {'I':1, 'V': 5, 'X':10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+        
+        for i in range(len(s)-1, -1, -1):
+            if i + 1 < len(s) and dic[s[i]] < dic[s[i+1]]:
+                res -= dic[s[i]]
+                
+            
+            else: res += dic[s[i]]
+               
+        return res
+            
             
         
