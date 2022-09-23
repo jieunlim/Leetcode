f374a1e764e3771c27eff6ffe2935851decf0510
J <zhiyinde@gmail.com>
2022-09-23 14:48:23 -0700

    Update 146-LRUCache.py

--- 146-LRUCache.py
+++ 146-LRUCache.py
@@ -58,7 +58,69 @@ class LRUCache:
         p.next = n
         n.prev = p
         
+# Solution2, add and remove function using key instead of node
+        
+class Node:
+    
+    def __init__(self, key=0, val=0):
+        self.val = val
+        self.key = key
+        self.next = None
+        self.prev = None
 
+class LRUCache:
+
+    def __init__(self, capacity: int):
+        self.capacity = capacity
+        self.dic = {}
+        
+        self.head = Node
+        self.tail = Node
+        self.head.next = self.tail
+        self.tail.prev = self.head
+        
+
+    def get(self, key: int) -> int:
+        if key in self.dic:
+            node = self.dic[key]
+            self.remove(key)
+            self.add(key)
+            return self.dic[key].val
+        return -1
+        
+        
+    def put(self, key: int, value: int) -> None:
+        if key in self.dic:
+            node = self.dic[key]
+            self.remove(key)
+        self.dic[key] = Node(key, value)
+        self.add(key)
+        
+        if len(self.dic) > self.capacity:
+            node = self.head.next
+            self.remove(node.key)
+            del self.dic[node.key]
+            
+        
+    def remove(self, key):
+        node = self.dic[key] 
+
+        p = node.prev
+        n = node.next
+
+        p.next = n
+        n.prev = p
+
+
+        
+    def add(self, key):
+        node = self.dic[key] 
+        self.tail.prev.next = node
+        node.prev = self.tail.prev
+        node.next = self.tail
+        self.tail.prev = node
+        
+        
 # Your LRUCache object will be instantiated and called as such:
 # obj = LRUCache(capacity)
 # param_1 = obj.get(key)
