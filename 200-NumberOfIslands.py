1415008caad7e64a6efdd17107387912353f613b
J <zhiyinde@gmail.com>
2022-09-25 00:05:25 -0700

    Update 200-NumberOfIslands.py

--- 200-NumberOfIslands.py
+++ 200-NumberOfIslands.py
@@ -1,6 +1,33 @@
 #T: O(nm), S: O(1)
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
+        n = len(grid)
+        m = len(grid[0])
+        dirs = [(1,0), (-1,0), (0,-1), (0,1)]
+        res = 0
+        
+        def dfs(r, c):
+            if 0 <= r < n and 0 <= c < m and grid[r][c] == "1":
+                grid[r][c] = '0'
+                
+                for x, y in dirs:
+                    nr = r + x
+                    nc = c + y
+                    
+                    dfs(nr, nc)
+                
+        for r in range(n):
+            for c in range(m):
+                if grid[r][c]=='1':
+                    dfs(r, c)
+                    res += 1
+                    
+        return res
+    
+    
+
+class Solution:
+    def numIslands(self, grid: List[List[str]]) -> int:
         res = 0
         r = len(grid)
         c = len(grid[0])
