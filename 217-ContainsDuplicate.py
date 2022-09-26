4d6064e996781aa2740ea61edf7dc407bb11d397
J <zhiyinde@gmail.com>
2022-09-25 20:01:36 -0700

    Update 217-ContainsDuplicate.py

--- 217-ContainsDuplicate.py
+++ 217-ContainsDuplicate.py
@@ -6,3 +6,19 @@ class Solution:
             if num in seen: return True
             seen.add(num)
         return False
+    
+    
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        return len(nums)!=len(set(nums))
+ 
+#T:O(Nlogn), S: O(1)
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        nums.sort()
+        curr = nums[0]
+        
+        for num in nums[1:]:
+            if num ^ curr == 0: return True
+            else: curr =num
+        return False
