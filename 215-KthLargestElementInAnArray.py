5272007783c9bc749b506b5eb33cb6a3a789a556
J <zhiyinde@gmail.com>
2022-09-25 18:00:42 -0700

    Quick Select, Quick Sort, Heap

--- 215-KthLargestElementInAnArray.py
+++ 215-KthLargestElementInAnArray.py
@@ -1,5 +1,32 @@
-
-
+#Quick Select, T: avg-O(n)
+from heapq import heappop
+class Solution:
+    def findKthLargest(self, nums: List[int], k: int) -> int:
+        #k번째로 큰거 : len(nums) - k 번째로 작은거
+        k= len(nums) - k
+        
+        def findKthSmallest(l, r):
+            pivot = partition(l, r)
+            if k < pivot:
+                return findKthSmallest(l, pivot-1)
+            if k > pivot:
+                return findKthSmallest(pivot + 1, r)
+            return nums[k]
+        
+        
+        #rightmost: pivot
+        def partition(l, r):
+            p = l
+            for i in range(l,r):
+                if nums[i]<=nums[r]:
+                    nums[i], nums[p] = nums[p], nums[i]
+                    p += 1
+            nums[p],nums[r]=nums[r],nums[p]
+            
+            return p
+        
+    
+        return findKthSmallest(0, len(nums)-1)
 
 #Min heap: T: O(Nlogk), S: O(k) for heap
     
