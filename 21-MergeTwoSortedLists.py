b6adb68a69299039b3576fb9854c9ed5ad1eb4f3
J <zhiyinde@gmail.com>
2022-10-02 14:56:34 -0700

    Update 21-MergeTwoSortedLists.py

--- 21-MergeTwoSortedLists.py
+++ 21-MergeTwoSortedLists.py
@@ -1,4 +1,26 @@
-
+# Time: O(L1 + L2), Space: O(1)
+class Solution:
+    def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
+        
+        if not list1 and not list2: return 
+        elif not list1 or not list2: return list1 or list2
+        
+       
+        dummy = cur = ListNode()
+        
+        while list1 and list2:
+            if list1.val < list2.val:
+                cur.next = list1
+                list1 = list1.next
+            else:
+                cur.next = list2
+                list2 = list2.next
+            cur = cur.next
+        cur.next = list1 or list2
+        
+        return dummy.next
+                
+            
 
 class Solution:
     def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
