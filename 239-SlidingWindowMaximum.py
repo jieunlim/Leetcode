ab78af98b507faa665511a23be4a333d96080cdb
J <zhiyinde@gmail.com>
2022-10-03 00:24:36 -0700

    Deque, Heap, Bruteforce

--- 239-SlidingWindowMaximum.py
+++ 239-SlidingWindowMaximum.py
@@ -1,4 +1,4 @@
-#Deque, Time: O(1), Space: O(1)
+#Deque, Time: O(N), Space: O(1)
 class Solution:
     def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
         dq = deque([])
@@ -13,6 +13,21 @@ class Solution:
             if i >= k-1:
                 res.append(nums[dq[0]])
             
+        return res
+#Heap, Time: O(Nlogk), Space: O(n) for heap
+class Solution:
+    def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
+        if k == 1: return nums
+        heap,  res = [], []
+        for i, val in enumerate(nums[:k]):
+            heappush(heap, (-val, i))
+        res = [-heap[0][0]]
+        
+        for i in range(k, len(nums)):
+            while heap[0][1] <= i-k:
+                heappop(heap)
+            heappush(heap, (-nums[i], i))
+            res.append(-heap[0][0])
         return res
                 
 # Time: O(N*K), Space: O(N-k+1)
