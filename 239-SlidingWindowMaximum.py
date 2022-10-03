6453f65b4d99e1a5cf65f790e0ac046957d98aaa
J <zhiyinde@gmail.com>
2022-10-03 00:00:48 -0700

    Create 239-SlidingWindowMaximum.py

---
+++ 239-SlidingWindowMaximum.py
@@ -0,0 +1,25 @@
+#Deque, Time: O(1), Space: O(1)
+class Solution:
+    def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
+        dq = deque([])
+        res = []
+        
+        for i, num in enumerate(nums):
+            print(f"i={i}, dq={dq}, res")
+            while dq and nums[dq[-1]] < num: 
+                dq.pop() # remove impossible candidate
+            dq.append(i)
+            if dq[0] == i-k: # out of the window
+                dq.popleft()
+            if i >= k-1:
+                res.append(nums[dq[0]])
+        return res
+                
+# Time: O(N*K), Space: O(N-k+1)
+class Solution:
+    def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
+        n = len(nums)
+        if n * k == 0:
+            return []
+        return [max(nums[i:i + k]) for i in range(n - k + 1)]
+                
