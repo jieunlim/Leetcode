00738d644b87e3e86f98a7666517e8dfcfd819ca
J <zhiyinde@gmail.com>
2022-11-13 21:31:00 -0800

    Update 55-Jump Game.py

--- 55-Jump Game.py
+++ 55-Jump Game.py
@@ -5,3 +5,14 @@ class Solution:
             if i + nums[i]>=idx:
                 idx = i
         return idx == 0
+#Greedy T: O(n), S: O(1)
+
+class Solution:
+    def canJump(self, nums: List[int]) -> bool:
+        idx = 0
+        for i in range(len(nums)):
+            if i > idx:
+                return False
+            idx = max(idx, i + nums[i])
+  
+        return  True
